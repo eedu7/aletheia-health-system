0b48015196611c341c621908d6e749b39328fc31
eedu7 <mueedahmad067@gmail.com>
2025-08-08 13:06:22 +0500

    Added the Sign In Form

--- frontend/src/app/auth/sign-in/page.tsx
+++ frontend/src/app/auth/sign-in/page.tsx
@@ -1,5 +1,9 @@
 import { Metadata } from "next";
 import React from "react";
+import { AuthCardFooter } from "@/app/auth/_components/AuthCardFooter";
+import { AuthCardHeader } from "@/app/auth/_components/AuthCardHeader";
+import { SignInForm } from "@/app/auth/sign-in/_components/SignInForm";
+import { Card, CardContent } from "@/components/ui/card";
 
 export const metadata: Metadata = {
 	title: "Sign In - Aletheia Health",
@@ -8,5 +12,13 @@ export const metadata: Metadata = {
 };
 
 export default function SignInPage() {
-	return <div>SignInPage</div>;
+	return (
+		<Card className="max-w-[400px] min-w-[300px]">
+			<AuthCardHeader />
+			<CardContent>
+				<SignInForm />
+			</CardContent>
+			<AuthCardFooter />
+		</Card>
+	);
 }
