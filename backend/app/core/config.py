466c92c676e9846a4045ebf9255bd220518925d4
eedu7 <mueedahmad067@gmail.com>
2025-08-10 11:13:55 +0500

    feat(db): add async SQLAlchemy engine and session setup
    - Created `Base` model using DeclarativeBase
    - Configured async engine with `create_async_engine`
    - Added async session factory using `async_sessionmaker`
    - Implemented `get_async_session` dependency for FastAPI

---
+++ backend/app/core/config.py
@@ -0,0 +1,45 @@
+from enum import StrEnum
+from pathlib import Path
+
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings
+
+
+class EnvironmentType(StrEnum):
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+    TEST = "test"
+
+
+class BaseConfig(BaseSettings):
+    class Config:
+        case_sensitive = True
+        env_file = Path(__file__).parent.parent.parent / ".env"
+        env_file_encoding = "utf-8"
+
+
+class Config(BaseConfig):
+    ENVIRONMENT: str = EnvironmentType.DEVELOPMENT
+    POSTGRES_DB: str = ""
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def POSTGRES_URL(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+
+# config: Config = Config()
+config: Config = Config()
+
+print(config.model_dump_json(indent=4))
