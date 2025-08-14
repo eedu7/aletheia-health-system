e17e3607a69e1a19b678bd30f0b61f07a669bd2e
eedu7 <mueedahmad067@gmail.com>
2025-08-14 10:10:59 +0500

    Added the pre-commit hook

--- backend/api/v1/health/health.py
+++ backend/api/v1/health/health.py
@@ -1,13 +1,10 @@
-import sys
-
+from fastapi import APIRouter, Depends
 from sqlalchemy import text
-import sys
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import APIRouter, Depends
 
 from core.database import get_async_session
-from pydantic import BaseModel
+
 router = APIRouter()
 
 
@@ -28,8 +25,3 @@ async def health(session: AsyncSession = Depends(get_async_session)):
         over_all_status = "error"
 
     return {"status": over_all_status, "services": services}
-
-
-class HealthCheck(BaseModel):
-    status: str
-    services: dict
