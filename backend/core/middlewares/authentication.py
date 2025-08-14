7853706d76f54594c158df280a1740b3649876bf
eedu7 <mueedahmad067@gmail.com>
2025-08-14 16:01:56 +0500

    feat(auth): add AuthBackend for JWT-based authentication with CurrentUser support

---
+++ backend/core/middlewares/authentication.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+
+from jose import JWTError
+from starlette.authentication import AuthenticationBackend
+from starlette.middleware.authentication import (
+    AuthenticationMiddleware as BaseAuthenticationMiddleware,
+)
+from starlette.requests import HTTPConnection
+
+from app.schemas.extra import CurrentUser
+from core.security import JWTHandler
+
+
+class AuthBackend(AuthenticationBackend):
+    async def authenticate(self, conn: HTTPConnection) -> Tuple[bool, CurrentUser]:
+        current_user = CurrentUser()
+        authorization: str = conn.headers.get("Authorization")
+
+        if not authorization:
+            return False, current_user
+
+        try:
+            scheme, token = authorization.split(" ")
+            if scheme.lower() != "bearer":
+                return False, current_user
+        except ValueError:
+            return False, current_user
+
+        if not token:
+            return False, current_user
+
+        try:
+            payload = JWTHandler.decode(token)
+            current_user.id = payload.get("id")
+        except JWTError:
+            return False, current_user
+
+        return True, current_user
+
+
+class AuthenticationMiddleware(BaseAuthenticationMiddleware):
+    pass
