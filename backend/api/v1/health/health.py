b91afde4891ca6b8ea7a5f125a595a1d403bbc8b
eedu7 <mueedahmad067@gmail.com>
2025-08-14 10:03:46 +0500

    Modified the health api and added the pre-commit

--- backend/api/v1/health/health.py
+++ backend/api/v1/health/health.py
@@ -1,13 +1,35 @@
-from fastapi import APIRouter, Depends
+import sys
+
 from sqlalchemy import text
+import sys
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends
 
 from core.database import get_async_session
-
+from pydantic import BaseModel
 router = APIRouter()
 
 
 @router.get("/")
 async def health(session: AsyncSession = Depends(get_async_session)):
-    message = await session.execute(text("SELECT 'ok'"))
-    return {"status": "ok", "services": {"database": message.scalar_one()}}
+    services = {}
+    try:
+        message = await session.execute(text("SELECT 'ok'"))
+        services["database"] = message.scalar_one()
+    except SQLAlchemyError as e:
+        services["database"] = f"Error: {e}"
+
+    if all(status == "ok" for status in services.values()):
+        over_all_status = "ok"
+    elif any(status == "ok" for status in services.values()):
+        over_all_status = "degraded"
+    else:
+        over_all_status = "error"
+
+    return {"status": over_all_status, "services": services}
+
+
+class HealthCheck(BaseModel):
+    status: str
+    services: dict
