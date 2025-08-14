832864dd66ba2b688a2a2801b0e77b98ba05257c
eedu7 <mueedahmad067@gmail.com>
2025-08-14 16:47:32 +0500

    feat(repository): add BaseRepository with common async CRUD and query utilities

--- backend/app/controllers/user.py
+++ backend/app/controllers/user.py
@@ -1,5 +1,3 @@
-from sqlalchemy import text
-
 from app.models import User
 from app.repositories import UserRepository
 from core.controller import BaseController
@@ -9,9 +7,3 @@ class AuthController(BaseController[User]):
     def __init__(self, user_repository: UserRepository) -> None:
         super().__init__(model=User, repository=user_repository)
         self.user_repository = user_repository
-
-    async def check_working(self):
-        result = await self.user_repository.session.execute(
-            text("SELECT 'Hello from Database!'")
-        )
-        return {"message": result.scalar_one()}
--- backend/core/controller/base.py
+++ backend/core/controller/base.py
@@ -1,15 +1,20 @@
 from typing import Any, Dict, Generic, Type, TypeVar
+from uuid import UUID
 
-from core.database import Base
+from core.database import Base, Propagation, Transactional
 from core.repository import BaseRepository
 
 ModelType = TypeVar("ModelType", bound=Base)
 
 
+# TODO: Add exception handling to the methods
 class BaseController(Generic[ModelType]):
     def __init__(self, model: Type[ModelType], repository: BaseRepository) -> None:
         self.model = model
         self.repository = repository
+
+    async def get_by_id(self, uuid: UUID) -> ModelType | None:
+        return await self.repository.session.get(uuid)
 
     async def get_all(self, skip: int = 0, limit: int = 100) -> list[ModelType]:
         try:
@@ -17,8 +22,6 @@ class BaseController(Generic[ModelType]):
         except Exception as e:
             raise e
 
-    async def create(self, attributes: Dict[str, Any]):
-        try:
-            return await self.repository.create(**attributes)
-        except Exception as e:
-            raise e
+    @Transactional(propagation=Propagation.REQUIRED)
+    async def create(self, attributes: Dict[str, Any]) -> ModelType:
+        return await self.repository.create(**attributes)
