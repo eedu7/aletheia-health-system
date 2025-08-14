2ae03eed378c1aab1058c329c99c2ce0e2fc5ca7
eedu7 <mueedahmad067@gmail.com>
2025-08-14 16:57:27 +0500

    feat(repository): add UserRepository with get_by_username and get_by_email methods

--- backend/app/repositories/user.py
+++ backend/app/repositories/user.py
@@ -3,4 +3,12 @@ from core.repository import BaseRepository
 
 
 class UserRepository(BaseRepository[User]):
-    pass
+    async def get_by_username(
+        self, username: str, join_: set[str] | None = None
+    ) -> User | None:
+        return await self.get_by("username", username, join_)
+
+    async def get_by_email(
+        self, email: str, join_: set[str] | None = None
+    ) -> User | None:
+        return await self.get_by("email", email, join_)
