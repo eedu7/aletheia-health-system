6bee080e88dc464b52c0d6d9fc9f242bbd096110
eedu7 <mueedahmad067@gmail.com>
2025-08-16 10:33:01 +0500

    feat(server): add init_listeners for custom exception handling
    - Introduced  in  to register global exception handlers
    - Added  for  returning JSON response
    - Ensured listeners are initialized during app creation

--- backend/core/server.py
+++ backend/core/server.py
@@ -1,15 +1,25 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import  JSONResponse
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
+from core.exceptions import CustomException
 from core.middlewares import AuthBackend, AuthenticationMiddleware, SQLAlchemyMiddleware
 
 
 def init_routers(app: FastAPI) -> None:
     app.include_router(router=router)
+
+def init_listeners(app: FastAPI) -> None:
+    @app.exception_handler(CustomException)
+    async def custom_exception_handler(request: Request, exc: CustomException):
+        return JSONResponse(
+            status_code=exc.code,
+            content={"error": exc.error_code, "message": exc.message},
+        )
 
 
 def make_middleware() -> List[Middleware]:
@@ -34,7 +44,7 @@ def create_app() -> FastAPI:
         middleware=make_middleware(),
     )
     init_routers(app=app)
-
+    init_listeners(app=app)
     return app
 
 
