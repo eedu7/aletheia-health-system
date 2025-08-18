2ea5c73b213aa6dfcb0d13e46a1fab5a54d4ce87
eedu7 <mueedahmad067@gmail.com>
2025-08-18 15:49:08 +0500

    feat(auth): add TokenType enum for access and refresh tokens

--- backend/app/schemas/extra/__init__.py
+++ backend/app/schemas/extra/__init__.py
@@ -1,5 +1,6 @@
 from .current_user import CurrentUser
 from .sender_type import SenderType
 from .token import Token
+from .token_type import TokenType
 
-__all__ = ["CurrentUser", "Token", "SenderType"]
+__all__ = ["CurrentUser", "Token", "SenderType", "TokenType"]
---
+++ backend/app/schemas/extra/token_type.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class TokenType(StrEnum):
+    ACCESS_TOKEN = "__HOST-al_hsat"
+    REFRESH_TOKEN = "__HOST-al_hsrt"
